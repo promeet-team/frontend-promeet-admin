4ed316292b1c9f316bee06763d7c6663808b41b5
mohamilin <amilinmoh99@gmail.com>
2020-11-18 13:07:13 +0700

    fix

--- src/redux/actions/bookingAction.js
+++ src/redux/actions/bookingAction.js
@@ -32,7 +32,7 @@ export function getEditBooking (data) {
 export const getBookingAction = ()  => {
     return async (dispatch) => {
         const response = await Axios
-        .get('http://server-promeet.herokuapp.com/api/admin/data/booking');
+        .get('https://server-promeet.herokuapp.com/api/admin/data/booking');
         dispatch(getBooking(response.data.booking));
         console.log('hasil ambil data booking', response.data.booking);
     }
@@ -41,7 +41,7 @@ export const getBookingAction = ()  => {
 export const getBookingDetailAction = (id) => {
     return async(dispatch) => {
         const response = await Axios 
-        .get(`http://server-promeet.herokuapp.com/api/admin/data/booking/${id}`);
+        .get(`https://server-promeet.herokuapp.com/api/admin/data/booking/${id}`);
         dispatch(getBookingDetail(response.data.booking));
         console.log('hasil detail', response.data.booking);
     }
@@ -52,7 +52,7 @@ export const getBookingDetailAction = (id) => {
         event.preventDefault();
         console.log('isi status', status)    
         return Axios
-        .put(`http://server-promeet.herokuapp.com/api/admin/edit-booking/${id}`, status)
+        .put(`https://server-promeet.herokuapp.com/api/admin/edit-booking/${id}`, status)
         .then((response) => {
             console.log('response edit', response.data.booking)
             dispatch(getEditBooking(response.data.booking))
